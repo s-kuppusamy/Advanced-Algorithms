dfda7198f10eaf6735fa49d1ff513ff5ecec8374
Shirin Kuppusamy <skuppusamy@olin.edu>
2021-05-11 09:12:10 -0400

    Partly Implemented Pseudocode and Logic Flow

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -79,8 +79,54 @@ class BBTreeNode():
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
 
-        #TODO: fill this part in
+        #So I wasn't really able to figure out fully how to implement This. Here is a combination of some pseudocode/thought process
+       
+        # Overall Algorithm Overview:
+            #you see what the result of the current iteration is, LP relaxing at each stage and solving
+            #if it's an integer, you like stop that iteration there
+            #else you break it into ceil and floor halves & redo the whole process of solving
+            #you iterate through all "done" parts to find the one with the best obj value
 
+        #Implementation/Pseudocode:
+
+        #WHILE LOOP: until each branch reaches an integer or is determine not feasible/not valuable, keep iterating
         
+            #check whether values in res of current branch are integers or not
+            if (root.is_integral()):
+                if (res >= bestres):
+                    bestres = res
+                    bestnode_vars = root.vars
+
+            else: #else if it is not already all integers
+                if (res >= bestres): #only proceed down a path if there is a better z-value
+                    #for each variable, if any one is not an integer, whichever one you run into first, branch out into the floor and ceil
+                    for v in root.vars[:-1]: 
+                        # can check if the variable is an int by seeing if round of the var is within tiny threshold of the float version of int)
+                        if v.value == None or abs(round(v.value) - float(v.value)) > 1e-4:
+                            floor = root.branch_floor(v)
+                            ceil = root.branch_ceil(v)
+                            # The next step is to use build problem and solve to find the res of floor and ceil
+                            
+                            floor_res = floor.buildProblem().solve(solver='cvxopt')
+                            ceil_res = ceil.buildProblem().solve(solver='cvxopt')
+                            #Unsure how to implement, but if these don't return a solution, need to have check to say it's infeasible and quit that path and move to another/the next branch 
+
+                            # Then you repeat lines 95-here on floor and ceil
+                            #Now, I didn't know how to actually "store" this for both floor and ceil to "keep them in the running"
+                            #My instinct from here was to recursively call the function with floor and ceil as root
+                            #but that would cause issues one 1- storage as I mentioned and 2- resetting the best_res & best_vars (not desired)
+                            #So per conversation with CA, one option is to a while loop instead where you keep iterating until each
+                            #branches reaches an integer or is determined infeasible (noted in line 92)
+                            #yet, I still don't know how to store these multiple res/root pairs -- do you add them to the heap? How? What does the heap look like? It won't let me print in a readable format.
+                            #another thought was to pick floor or ceil, go all the way down that path until you hit an integer or infeasible 
+                            #and then return back up the last place you branched coming all the way back up, but again still don't know how to store
+                            #and also for the while loop, didn't quite know how to set up that while loop
+
         return bestres, bestnode_vars
+
+
+     
+
  
+
+
